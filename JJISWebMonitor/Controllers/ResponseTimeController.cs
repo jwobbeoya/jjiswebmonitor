c587eee2cad38713661b39d8552f6de48c1387a2
Jeff Wobbe <wobbej@oya.state.or.us>
2019-06-28 08:35:41 -0700

    Moved to .NET Framework

---
+++ JJISWebMonitor/Controllers/ResponseTimeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JJISWebMonitor.Controllers
+{
+    public class ResponseTimeController : ApiController
+    {
+       public string GetResponseTime()
+       {
+         var webRequest = (HttpWebRequest)WebRequest.Create(new Uri("https://www.jjis.oregon.gov/staticcontent/connectivitytest.html"));
+
+         var sw = Stopwatch.StartNew();
+
+         var response = (HttpWebResponse)webRequest.GetResponse();
+         using (var responseStream = response.GetResponseStream())
+         {
+            if (responseStream == null)
+               return $"Response was null.  Response: {response.StatusCode} {response.StatusDescription}";
+
+            using (var sr = new StreamReader(responseStream))
+            {
+               sr.ReadToEnd();
+            }
+
+         }
+
+         sw.Stop();
+
+         return $"{sw.ElapsedMilliseconds} ms";
+      }
+    }
+}
