82e85a7f830c339daa950301273569a998025c68
Jeff Wobbe <wobbej@oya.state.or.us>
2019-06-28 08:04:38 -0700

    Init

---
+++ JJISWebMonitor/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JJISWebMonitor.Controllers
+{
+   [Route("api/[controller]")]
+   [ApiController]
+   public class MonitorController : ControllerBase
+   {
+      // GET api/values
+      [HttpGet]
+      public ActionResult<string> Get()
+      {
+         var webRequest = (HttpWebRequest) WebRequest.Create(new Uri("https://www.jjis.oregon.gov/staticcontent/connectivitytest.html"));
+
+         var sw = Stopwatch.StartNew();
+
+         var response =  (HttpWebResponse) webRequest.GetResponse();
+         using (var sr = new StreamReader(response.GetResponseStream()))
+         {
+            sr.ReadToEnd();
+         }
+         
+         sw.Stop();
+
+         return $"{sw.ElapsedMilliseconds} ms";
+      }
+   }
+}
