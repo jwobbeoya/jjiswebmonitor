b74fa9e0542d97cd02e6ced8695ffe83711a2157
Jeff Wobbe <wobbej@oya.state.or.us>
2019-07-16 11:08:18 -0700

    show host

--- JJISWebMonitor/script.js
+++ JJISWebMonitor/script.js
@@ -5,7 +5,7 @@
       var host = document.querySelector("#host").value;
       var response = await fetch(`/api/responsetime/connect?host=${host}`);
       var output = document.querySelector('#output');
-      output.innerHTML = output.innerHTML + `<div>${await response.json()}</div>`;
+      output.innerHTML = output.innerHTML + `<div>${await response.json()} - ${host}</div>`;
    } finally {
       button.disabled = "";
    }
