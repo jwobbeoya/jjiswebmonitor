9e8302479e0def30cf315ed41db4d2f9738c7c48
Jeff Wobbe <wobbej@oya.state.or.us>
2019-07-16 10:08:13 -0700

    Added UI

---
+++ JJISWebMonitor/script.js
@@ -0,0 +1,6 @@
+﻿async function measure() {
+   var host = document.querySelector("#host").value;
+   var response = await fetch(`/api/responsetime/connect?host=${host}`);
+   var output = document.querySelector('#output');
+   output.innerHTML = output.innerHTML + `<div>${await response.json()}</div>`;
+}
