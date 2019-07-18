f96e0295d529f8340fbc34658cf11f3374aab109
Jeff Wobbe <wobbej@oya.state.or.us>
2019-07-18 14:05:56 -0700

    Http request tool

---
+++ JJISWebMonitor/script/http.js
@@ -0,0 +1,31 @@
+﻿async function measure() {
+   event.preventDefault();
+   var button = document.querySelector("#btnFetch");
+   try {
+      button.disabled = "disabled";
+      button.value = "Fetch";
+      var uri = document.querySelector("#uri").value;
+      var response = await fetch(`/api/responsetime/http?uri=${uri}`);
+      var output = document.querySelector('#output');
+      output.innerHTML = output.innerHTML + `<div>${await response.json()} - ${uri}</div>`;
+   } finally {
+      button.disabled = "";
+      button.value = "Fetch";
+   }
+}
+
+function pageLoaded() {
+   setInputFromQuery("uri");
+}
+
+function setInputFromQuery(name) {
+   var value = new URLSearchParams(document.location.search).get(name);
+   if (value !== null && value !== '')
+      document.querySelector("#" + name).value = value;
+   
+}
+
+function clearOutput() {
+   var output = document.querySelector('#output');
+   output.innerHTML = "";
+}
